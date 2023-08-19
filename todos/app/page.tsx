75ff7a2dafa920d787a3363969a981d29547ee79
DESKTOP-7V9Q99B\AKU <akuraza6atgmail.com>
2023-08-19 11:19:02 +0500

    testing project

---
+++ todos/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+export default function Home() {
+  
+  return (
+    
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+    
+
+      hi testing 123123
+      
+    </main>
+  )
+}
